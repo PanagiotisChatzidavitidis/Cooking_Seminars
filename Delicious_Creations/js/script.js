2560241d27059df4cf29b1c305e5e2fc332c8d0a
DamnedThing <kmanolakis11@gmail.com>
2023-06-19 05:37:05 +0300

    Dark Mode-Pop Up Windows

---
+++ Delicious_Creations/js/script.js
@@ -0,0 +1,19 @@
+function confirmSignOut() {
+    var confirmLogout = window.confirm("Are you sure you want to sign out?");
+  
+    if (confirmLogout) {
+      window.location.href = "home.html"; // Replace with your actual sign-out URL
+    } else {
+      // User clicked 'Cancel' - do nothing
+    }
+  }
+
+  $(document).ready(function() {
+    $('#toggleDarkMode').click(function() {
+      $('body').toggleClass('dark-mode');
+    });
+  });
+
+  
+
+  
