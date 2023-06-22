4d76e25a0c4b47dfe1b9b48835a8f6925509f709
DamnedThing <kmanolakis11@gmail.com>
2023-06-22 06:33:21 +0300

    Session Creation/Sign-in/Sign-Out

--- Delicious_Creations/js/script.js
+++ Delicious_Creations/js/script.js
@@ -1,12 +1,30 @@
 function confirmSignOut() {
-    var confirmLogout = window.confirm("Are you sure you want to sign out?");
-  
-    if (confirmLogout) {
-      window.location.href = "home.html"; // Replace with your actual sign-out URL
-    } else {
-      // User clicked 'Cancel' - do nothing
-    }
+  var confirmLogout = window.confirm("Are you sure you want to sign out?");
+
+  if (confirmLogout) {
+    fetch('http://localhost:3000/users/signout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Session destroyed'); // Log session destroyed
+          window.location.href = "home.html"; // Replace with your actual sign-out URL
+        } else {
+          console.log('Error destroying session');
+        }
+      })
+      .catch(error => {
+        console.error('Error signing out:', error);
+      });
+  } else {
+    // User clicked 'Cancel' - do nothing
   }
+}
+
+
 
   $(document).ready(function() {
     $('#toggleDarkMode').click(function() {
